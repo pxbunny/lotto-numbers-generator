45a8113e8296eb7c63d783c86f871bd3c4b3f4cd
dae-ne <rafalcz29@gmail.com>
2024-10-28 09:19:13 +0100

    Use random delay between selected numbers

--- lotto.py
+++ lotto.py
@@ -1,20 +1,29 @@
 import random
 import time
 
+MIN_NUMBER = 1
+MAX_NUMBER = 49
+
+
+def get_number():
+    number = random.randint(MIN_NUMBER, MAX_NUMBER)
+
+    while number in all_numbers:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+
+    return number
+
+
 all_numbers = []
 
 print('Welcome to the Lotto Number Generator!')
 print('Here are your lucky numbers:\n')
 
 for i in range(1, 7):
-    number = random.randint(1, 49)
-
-    while number in all_numbers:
-        number = random.randint(1, 49)
-
+    number = get_number()
     all_numbers.append(number)
     print(number)
-    time.sleep(1)
+    time.sleep(random.uniform(0.5, 1.5))
 
 print('\nYour lucky numbers are: ', end='')
 all_numbers.sort()
