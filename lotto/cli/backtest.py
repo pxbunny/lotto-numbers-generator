c60365d66a5672e30e93245228c27b4431f2220c
dae-ne <rafalcz29@gmail.com>
2025-09-30 15:34:42 +0200

    Calculate metrics

--- lotto/cli/backtest.py
+++ lotto/cli/backtest.py
@@ -1,6 +1,9 @@
 import typer
 
+from lotto.models import GameType
+
 from ..data.api_mock import get_data
+from ..metrics import MetricsCalculator
 from ..simulation import BacktestEngine
 from ..visualisation import visualise_results
 
@@ -12,4 +15,18 @@ def run_backtest(skip_plus: bool = False):
     backtest = BacktestEngine()
     data = get_data()
     backtest.run(data, skip_plus)
+
+    metrics_calculator = MetricsCalculator(backtest.history)
+    lotto_metrics = metrics_calculator.generate_report(GameType.LOTTO)
+    lotto_plus_metrics = metrics_calculator.generate_report(GameType.LOTTO_PLUS)
+
+    print(f'Basic metrics for Lotto: {lotto_metrics.basic_accuracy}')
+    print(f'Basic metrics for Lotto Plus: {lotto_plus_metrics.basic_accuracy}')
+
+    print(f'Monetary metrics for Lotto: {lotto_metrics.monetary_metrics}')
+    print(f'Monetary metrics for Lotto Plus: {lotto_plus_metrics.monetary_metrics}')
+
+    print(f'Statistical quality metrics for Lotto: {lotto_metrics.statistical_quality}')
+    print(f'Statistical quality metrics for Lotto Plus: {lotto_plus_metrics.statistical_quality}')
+
     visualise_results(backtest.history)
