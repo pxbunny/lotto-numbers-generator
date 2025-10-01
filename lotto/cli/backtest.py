6c62bb3f89d7600e8715c775c62f85bae33e70ee
dae-ne <rafalcz29@gmail.com>
2025-10-01 11:08:31 +0200

    Add vscode recommended extensions

--- lotto/cli/backtest.py
+++ lotto/cli/backtest.py
@@ -1,9 +1,8 @@
 import typer
 
-from lotto.models import GameType
-
 from ..data.api_mock import get_data
 from ..metrics import MetricsCalculator
+from ..models import GameType
 from ..simulation import BacktestEngine
 from ..visualisation import visualise_results
 
@@ -16,17 +15,17 @@ def run_backtest(skip_plus: bool = False):
     data = get_data()
     backtest.run(data, skip_plus)
 
-    metrics_calculator = MetricsCalculator(backtest.history)
-    lotto_metrics = metrics_calculator.generate_report(GameType.LOTTO)
-    lotto_plus_metrics = metrics_calculator.generate_report(GameType.LOTTO_PLUS)
+    # metrics_calculator = MetricsCalculator(backtest.history)
+    # lotto_metrics = metrics_calculator.generate_report(GameType.LOTTO)
+    # lotto_plus_metrics = metrics_calculator.generate_report(GameType.LOTTO_PLUS)
 
-    print(f'Basic metrics for Lotto: {lotto_metrics.basic_accuracy}')
-    print(f'Basic metrics for Lotto Plus: {lotto_plus_metrics.basic_accuracy}')
+    # print(f'Basic metrics for Lotto: {lotto_metrics.basic_accuracy}')
+    # print(f'Basic metrics for Lotto Plus: {lotto_plus_metrics.basic_accuracy}')
 
-    print(f'Monetary metrics for Lotto: {lotto_metrics.monetary_metrics}')
-    print(f'Monetary metrics for Lotto Plus: {lotto_plus_metrics.monetary_metrics}')
+    # print(f'Monetary metrics for Lotto: {lotto_metrics.monetary_metrics}')
+    # print(f'Monetary metrics for Lotto Plus: {lotto_plus_metrics.monetary_metrics}')
 
-    print(f'Statistical quality metrics for Lotto: {lotto_metrics.statistical_quality}')
-    print(f'Statistical quality metrics for Lotto Plus: {lotto_plus_metrics.statistical_quality}')
+    # print(f'Statistical quality metrics for Lotto: {lotto_metrics.statistical_quality}')
+    # print(f'Statistical quality metrics for Lotto Plus: {lotto_plus_metrics.statistical_quality}')
 
     visualise_results(backtest.history)
